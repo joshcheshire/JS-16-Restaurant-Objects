5d162ebf39ea1e150f9f35a7ef0cdbfabc367d73
Student <student@RU-017.local>
2015-09-21 17:32:06 -0600

    end of day

--- main.js
+++ main.js
@@ -16,19 +16,24 @@ var Fooditem = function( name, calories, vegan, glutenFree, citrusFree ){
 
 Fooditem.prototype.stringify = function(){
 
-	console.log((this.name + ' ' + this.calories + ' ' +  'is this vegan? ' +  this.vegan + ' ' + this.glutenFree + ' ' + this.citrusFree))
+	return this.name + ' ' + this.calories + ' ' +  'is this vegan? ' +  this.vegan + ' ' + this.glutenFree + ' ' + this.citrusFree	
 
 }
 
 
 var burrito = new Fooditem( 'Burrito', 500, false, false, true)
- var pizza = new Fooditem('Pizza', 600, false, true, true)
- var burger = new Fooditem( 'Burger', 1000, false, false, true)
+ var rice = new Fooditem('rice', 200, true, true, true)
+ var beans = new Fooditem( 'beans',400, true, true, true)
+ var avocado = new Fooditem( 'avocado', 1000, true, false, true)
+ var tequila = new Fooditem ('tequila', 500, true, true, false)
+ var lemon = new Fooditem ('lemon', 50, true, true, false)
+
 
 
 // burrito.stringify()
 // pizza.stringify()
 // burger.stringify()
+// lemon.stringify()
 
 
 
@@ -44,13 +49,13 @@ var Drink = function( name, description, price, ingredients){
 	this.ingredients = ingredients;
 
 
-
-
 }
+
+
 
 Drink.prototype.stringify = function(){
 
-console.log((this.name + ' ' + this.description + ' ' + this.price + ' ' + this.ingredients))
+return this.name + ' ' + this.description + ' ' + this.price + ' ' + this.ingredients
 
 
 
@@ -64,14 +69,18 @@ var Plate = function( name, description, price, ingredients){
 	this.description= description;
 	this.price = price;
 	this.ingredients = ingredients;
-
+	
 }
 
 
 
 Plate.prototype.stringify = function(){
+var foodItemString = ''
+this.ingredients.forEach(function(item){
+	foodItemString += item.stringify()	
 
-console.log((this.name + ' ' + this.description + ' ' + this.price + ' ' + this.ingredients))
+})
+return this.name + ' ' + this.description + ' ' + this.price + ' ' + foodItemString
 
 }
 
@@ -80,38 +89,114 @@ console.log((this.name + ' ' + this.description + ' ' + this.price + ' ' + this.
 var coke = new Drink ('Cola', 'Fizzy and yummy', 2, ['corn syrup', 'caramel'])
 // console.log(coke)
 
+var Marg = new Drink ('Margarita', 'booze slushy', 8, [tequila,lemon])
 
 var italianDinner = new Plate ('Spaghetti', ' Italian deliciousness', 15, ['noodles', 'meatballs', 'sauce', 'garlic bread'])
 // console.log(italianDinner)
 
 var chickenDinner = new Plate ('Chicken Dinner', 'southern deliciousness', 18,['chicken', 'green beans', 'mashed potatoes', 'gravy'])
 
-// coke.stringify()
-// italianDinner.stringify()
-// chickenDinner.stringify()
+var burritoPlate = new Plate ('Burrito Plate', 'deliciousness', 10, [burrito, rice, beans])
 
+// coke.stringify()
+// chickenDinner.stringify()
+// burritoPlate.stringify()
+console.log(burritoPlate)
+console.log(italianDinner.stringify())
+console.log(burritoPlate.stringify())
 
 
 //=-=-=-=--=-=-=-=-=-=-=ORDER-=-=-=-=-=\\
 
 
-var Order = function(plate){
-	this.plate = plate;
-
-
+var Order = function(arr){
+	this.arr = arr
 }
+
+
 
 Order.prototype.stringify = function(){
 
-console.log(this.plate.stringify())
+	var orderString = ''
+	this.arr.forEach(function(plates){
+		orderString = orderString + plates.stringify();
+	})
+	return orderString
+
+}
+
+
+
+var myOrder = new Order([italianDinner,chickenDinner])
+
+
+// console.log(myOrder.stringify())
+
+//=-=-=-=-=-=-=Menu-=-=-=-=-=\\
+
+var Menu = function(arr){
+	this.arr = arr
+}
+
+Menu.prototype.stringify =  function(){
+
+	var orderString = ''
+	this.arr.forEach(function(plates){
+		orderString = orderString + plates.stringify();
+		
+
+	})
+	return orderString
+}
+
+
+var ourMenu = new Menu([italianDinner,chickenDinner])
+
+// console.log(ourMenu.stringify())
+
+
+//=-=-=-=-=-=-Restaurant-=-=-=-=-=\\
+
+var Restaurant = function(name, description, menu){
+	this.name = name
+	this.description= description
+	this.menu = menu
+
+}
+
+Restaurant.prototype.stringify = function(){
+
+
+	var restString = ''
+	this.menu.arr.forEach(function(plates){
+		restString = restString + plates.stringify()
+
+	})
+
+return this.name + ' ' + this.description + ' ' + restString
+
+}
+
+var theRestaurant = new Restaurant ('Our Restaurant', 'Homestyle Eats', ourMenu)
+
+
+// console.log(theRestaurant.stringify())
+
+
+
+
+
+
+var Customer = function(dietaryPreference){
+
+	this.dietaryPreference = dietaryPreference
+
 
 
 }
 
-var myOrder = new Order([italianDinner,chickenDinner])
 
-myOrder.stringify()
-
+var theCustomer = new Customer ('Vegan')
 
 
 
